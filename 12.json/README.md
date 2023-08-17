9ea714e239b3418da4dc7cde764f0ab24de0be18
eoeung <eoeung113@gmail.com>
2023-08-17 20:27:00 +0900

    add JSON

---
+++ 12.json/README.md
@@ -0,0 +1,8 @@
+# 12.JSON (2023/08/16)
+
+## [JSON]
+
+- JSON.parse(object)
+- JSON.stringify(JSON)
+---
+
--- 12.json/json.js
+++ 12.json/json.js
@@ -7,3 +7,19 @@ const car = `{
     "isElectricCar": true,
     "options": ["side mirror", "smart sensor", "built-in cam" ]
 }`;
+
+// JSON내장 객체의 parse랑 stringify
+let jp = JSON.parse(car);
+console.log(jp);
+// {
+//     model: 'IONIQ 5',
+//     company: 'HYUNDAI',
+//     price: 50000000,
+//     year: 2023,
+//     isElectricCar: true,
+//     options: [ 'side mirror', 'smart sensor', 'built-in cam' ]
+//   }
+
+let js = JSON.stringify(jp);
+console.log(js);
+// {"model":"IONIQ 5","company":"HYUNDAI","price":50000000,"year":2023,"isElectricCar":true,"options":["side mirror","smart sensor","built-in cam"]}
