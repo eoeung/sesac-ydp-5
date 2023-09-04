8063602656adcfb89e354224eb9cd7176b4f4358
eoeung <eoeung113@gmail.com>
2023-09-04 15:16:07 +0900

    add env

---
+++ 19.env/README.md
@@ -0,0 +1,8 @@
+# 19.env(2023/09/04)
+
+## [env]
+
+- 환경 변수
+  - dotenv
+  - dotenv.config()
+  - process.env.key
---
+++ 19.env/index.js
@@ -0,0 +1,19 @@
+// const ps = process.env;
+// console.log(ps); {...}
+
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  console.log(process.env.NAME); // sesac
+  console.log(process.env.NODE); // dev
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT} is running`);
+});
